74cac353b37716e6656c994002691961a6db6754
hz-heng <hz_heng@qq.com>
2017-06-29 14:02:07 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 import os
 
-#the version
+#版本号设置
 VERSION = 'Ver: 1.0.2'
 
-#csrf settings
+#csrf设置
 CSRF_ENABLED = True
 SECRET_KEY = 'abxdefghuijk'
 
-#database settings
+#数据库设置
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, 'sms.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 
-#alidayu settings
+#阿里大于api设置
 URL = "http://gw.api.taobao.com/router/rest"
-APPKEY = ""
-SECRET = ""
+APPKEY = "" #填入自己的APPKEY
+SECRET = "" #填入自己的SECRET
 
